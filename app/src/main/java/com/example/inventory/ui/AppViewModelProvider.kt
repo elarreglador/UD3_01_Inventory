b767b24957d084e4ece2b2f476e86cb3fdf3e676
elarreglador <elarreglador@protonmail.com>
2024-12-22 09:06:57 +0100

    ✨ Cambio menor: 6. Muestra los detalles del element

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -46,7 +46,8 @@ object AppViewModelProvider {
         // Initializer for ItemDetailsViewModel
         initializer {
             ItemDetailsViewModel(
-                this.createSavedStateHandle()
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
             )
         }
 
